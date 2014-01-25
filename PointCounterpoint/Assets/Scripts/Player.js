f5f7a470dd0fb2a5a3473757ab7218d6ba3ae7bc
Adam Gressen <adamgressen@gmail.com>
2014-01-24 22:34:33 -0500

    player movement

--- PointCounterpoint/Assets/Scripts/Player.js
+++ PointCounterpoint/Assets/Scripts/Player.js
@@ -1,15 +1,29 @@
 ﻿#pragma strict
 
-public var playerSpeed = 5;
+public var playerSpeed = 10;
 
 function Start () {
 	
 }
 
 function FixedUpdate () {
+	// move left
 	if (Input.GetKey(KeyCode.A)) {
-		
+		rigidbody2D.transform.position -= Vector2.right * playerSpeed * Time.deltaTime;
 	}
-	if (Input.GetKey(KeyCode.D)) {
+	
+	// move right
+	else if (Input.GetKey(KeyCode.D)) {
+		rigidbody2D.transform.position += Vector2.right * playerSpeed * Time.deltaTime;
+	}
+	
+	// move up
+	if (Input.GetKey(KeyCode.W)) {
+		rigidbody2D.transform.position += Vector2.up * playerSpeed * Time.deltaTime;
+	}
+	
+	// move down
+	else if (Input.GetKey(KeyCode.S)) {
+		rigidbody2D.transform.position -= Vector2.up * playerSpeed * Time.deltaTime;
 	}
 }
