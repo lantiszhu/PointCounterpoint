c8f7cf0dd2f8db0bb6bd90d83fbbcf5be8ea4c65
Eddie <oneup@bu.edu>
2014-01-24 22:34:32 -0500

    Clamping.

---
+++ PointCounterpoint/Assets/Scripts/PlayerClamp.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var foobar = "REPLACE ME WITH A VARIABLE"
+public var leftClamp = foobar;
+public var rightClamp = foobar;
+
+function Start () {
+
+}
+
+function Update () {
+	
+	if (rigidbody2D.transform.position < leftClamp || rigidbody2D.transform.position > rightClamp) {
+		Debug.Log("Clamped.")
+		
+		Vector3 pos = rigidbody2D.transform.position;
+		
+		pos.x = Math.Clamp(pos.x, leftClamp, rightClamp);
+		
+		rigidbody2D.transform.position = pos;
+	}
+
+}
