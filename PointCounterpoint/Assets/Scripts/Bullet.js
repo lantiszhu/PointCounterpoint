c5d95fd93bfd9064a301ba90640dce8cade83ae3
Adam Gressen <adamgressen@gmail.com>
2014-01-24 23:30:42 -0500

    shooting bullets upwards and destroying them

---
+++ PointCounterpoint/Assets/Scripts/Bullet.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+public var speed = 30;
+public var playerScript : Player;
+
+function Start () {
+	playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent(Player);
+}
+
+function Update () {
+	transform.position += Vector2.up * speed * Time.deltaTime;
+	
+	if (transform.position.y > playerScript.topClamp) {
+		//Debug.Log(transform.position.y);
+		//Debug.Log(playerScript.topClamp);
+		Destroy(gameObject);
+	}
+}
--- PointCounterpoint/Assets/Scripts/Player.js
+++ PointCounterpoint/Assets/Scripts/Player.js
@@ -1,9 +1,19 @@
 ﻿#pragma strict
 
 public var playerSpeed = 10;
+public var dist;
+public var leftClamp;
+public var rightClamp;
+public var topClamp : float;
+public var bottomClamp;
+public var bulletPrefab : GameObject;
 
 function Start () {
-	
+	dist = (transform.position - Camera.main.transform.position).z;
+	leftClamp = Camera.main.ViewportToWorldPoint(Vector3(0, 0, dist)).x;
+	rightClamp = Camera.main.ViewportToWorldPoint(Vector3(1, 0, dist)).x;
+	topClamp = Camera.main.ViewportToWorldPoint(Vector3(0, 1, dist)).y;
+	bottomClamp = Camera.main.ViewportToWorldPoint(Vector3(0, 0, dist)).y;
 }
 
 function FixedUpdate () {
@@ -26,4 +36,11 @@ function FixedUpdate () {
 	else if (Input.GetKey(KeyCode.S)) {
 		rigidbody2D.transform.position -= Vector2.up * playerSpeed * Time.deltaTime;
 	}
+	
+	if (Input.GetKey(KeyCode.LeftArrow)) {
+		Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+	}
+	
+	transform.position.x = Mathf.Clamp(transform.position.x, leftClamp, rightClamp);
+	transform.position.y = Mathf.Clamp(transform.position.y, bottomClamp, topClamp);
 }
