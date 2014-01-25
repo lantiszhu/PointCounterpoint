09c214be7d0379ec6200bcab609438e95da5bf50
Adam Gressen <adamgressen@gmail.com>
2014-01-24 22:08:54 -0500

    scripts folder, player script

---
+++ PointCounterpoint/Assets/Scripts/Player.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var playerSpeed = 5;
+
+function Start () {
+	
+}
+
+function FixedUpdate () {
+	if (Input.GetKey(KeyCode.A)) {
+		
+	}
+	if (Input.GetKey(KeyCode.D)) {
+	}
+}
