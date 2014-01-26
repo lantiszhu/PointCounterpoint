9eba8ce155560d896070c63b1c4d5d58f3980a71
Adam Gressen <adamgressen@gmail.com>
2014-01-26 13:19:16 -0500

    intro screen

--- PointCounterpoint/Assets/Scripts/Intro.js
+++ PointCounterpoint/Assets/Scripts/Intro.js
@@ -1,11 +1,23 @@
 ﻿#pragma strict
 
+var mySkin : GUISkin;
+
+var introImage : Texture;
+
 function Start () {
 
 }
 
 function Update () {
 	if (Input.GetKey(KeyCode.Space)) {
-		
+		Application.LoadLevel(Application.loadedLevel + 1);
 	}
+}
+
+function OnGUI() {
+	GUI.skin = mySkin;
+	
+	GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), introImage);
+	
+	GUI.Label(new Rect(Screen.width / 2 - 150, Screen.height - 50, 300, 100), "Press Start");
 }
